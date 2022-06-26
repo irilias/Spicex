c3acce7a87ed77f82b643af550a10da870c2257d
Ilyas Rima <irilias@gmail.com>
2022-06-26 21:48:06 +0200

    Initial commit

---
+++ Spicex/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Spicex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
